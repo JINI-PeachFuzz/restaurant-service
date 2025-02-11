8a1c8091e443cf034d7aaaa5e192edcd7fa95f5c
JINJU PARK <aricclunj@naver.com>
2025-02-11 22:25:36 +0900

    .

--- src/main/java/org/jiniit/restaurant/controllers/NeighborSearch.java
+++ src/main/java/org/jiniit/restaurant/controllers/NeighborSearch.java
@@ -1,9 +1,9 @@
-package org.jiniit.restaurant.controllers;
+package org.koreait.restaurant.controllers;
 
 import lombok.Data;
 
 @Data
-public class NeighborSearch {
+public class NeighborSearch extends RestaurantSearch {
     private double lat;
     private double lon;
     private int limit;
